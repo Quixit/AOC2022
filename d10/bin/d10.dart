6cfcd3c07e4d544ff0dc5e7f41fa03066c65803f
Jonathan Bruce <jonathanb@spswave.com>
2022-12-10 12:10:39 -0500

    ten

---
+++ d10/bin/d10.dart
@@ -0,0 +1,240 @@
+import 'package:d10/d10.dart';
+import 'dart:collection';
+
+import 'package:test/expect.dart';
+
+final sample = [
+  "addx 15",
+  "addx -11",
+  "addx 6",
+  "addx -3",
+  "addx 5",
+  "addx -1",
+  "addx -8",
+  "addx 13",
+  "addx 4",
+  "noop",
+  "addx -1",
+  "addx 5",
+  "addx -1",
+  "addx 5",
+  "addx -1",
+  "addx 5",
+  "addx -1",
+  "addx 5",
+  "addx -1",
+  "addx -35",
+  "addx 1",
+  "addx 24",
+  "addx -19",
+  "addx 1",
+  "addx 16",
+  "addx -11",
+  "noop",
+  "noop",
+  "addx 21",
+  "addx -15",
+  "noop",
+  "noop",
+  "addx -3",
+  "addx 9",
+  "addx 1",
+  "addx -3",
+  "addx 8",
+  "addx 1",
+  "addx 5",
+  "noop",
+  "noop",
+  "noop",
+  "noop",
+  "noop",
+  "addx -36",
+  "noop",
+  "addx 1",
+  "addx 7",
+  "noop",
+  "noop",
+  "noop",
+  "addx 2",
+  "addx 6",
+  "noop",
+  "noop",
+  "noop",
+  "noop",
+  "noop",
+  "addx 1",
+  "noop",
+  "noop",
+  "addx 7",
+  "addx 1",
+  "noop",
+  "addx -13",
+  "addx 13",
+  "addx 7",
+  "noop",
+  "addx 1",
+  "addx -33",
+  "noop",
+  "noop",
+  "noop",
+  "addx 2",
+  "noop",
+  "noop",
+  "noop",
+  "addx 8",
+  "noop",
+  "addx -1",
+  "addx 2",
+  "addx 1",
+  "noop",
+  "addx 17",
+  "addx -9",
+  "addx 1",
+  "addx 1",
+  "addx -3",
+  "addx 11",
+  "noop",
+  "noop",
+  "addx 1",
+  "noop",
+  "addx 1",
+  "noop",
+  "noop",
+  "addx -13",
+  "addx -19",
+  "addx 1",
+  "addx 3",
+  "addx 26",
+  "addx -30",
+  "addx 12",
+  "addx -1",
+  "addx 3",
+  "addx 1",
+  "noop",
+  "noop",
+  "noop",
+  "addx -9",
+  "addx 18",
+  "addx 1",
+  "addx 2",
+  "noop",
+  "noop",
+  "addx 9",
+  "noop",
+  "noop",
+  "noop",
+  "addx -1",
+  "addx 2",
+  "addx -37",
+  "addx 1",
+  "addx 3",
+  "noop",
+  "addx 15",
+  "addx -21",
+  "addx 22",
+  "addx -6",
+  "addx 1",
+  "noop",
+  "addx 2",
+  "addx 1",
+  "noop",
+  "addx -10",
+  "noop",
+  "noop",
+  "addx 20",
+  "addx 1",
+  "addx 2",
+  "addx 2",
+  "addx -6",
+  "addx -11",
+  "noop",
+  "noop",
+  "noop",
+]
+    .map((e) => e.split(" "))
+    .map((e) => Instruction(e[0], e.length > 1 ? int.parse(e[1]) : 0))
+    .toList();
+
+final input = readInput()
+    .map((e) => e.split(" "))
+    .map((e) => Instruction(e[0], e.length > 1 ? int.parse(e[1]) : 0))
+    .toList();
+
+class Instruction {
+  Instruction(this.operation, this.value);
+
+  String operation;
+  int value;
+}
+
+int getSignals(List<int> breaks, bool draw) {
+  var sum = 0;
+  var x = 1;
+  var cycle = 1;
+  var buffer = "";
+
+  for (var instruction in input) {
+    var processTime = 0;
+
+    switch (instruction.operation) {
+      case "noop":
+        processTime = 1;
+        break;
+      case "addx":
+        processTime = 2;
+        break;
+    }
+
+    //simulate cycles
+    for (var i = 0; i < processTime; i++) {
+      if (breaks.contains(cycle)) {
+        sum += cycle * x;
+
+        if (draw) {
+          print(buffer);
+
+          buffer = "";
+        }
+      }
+
+      if (draw) {
+        var cursor = (cycle - 1) % 40;
+
+        if (x <= cursor + 1 && x >= cursor - 1) {
+          buffer += "#";
+        } else {
+          buffer += ".";
+        }
+      }
+      cycle++;
+    }
+
+    //finish operation
+    switch (instruction.operation) {
+      case "noop":
+        //nothing
+        break;
+      case "addx":
+        x += instruction.value;
+        break;
+    }
+  }
+
+  if (draw) {
+    print(buffer);
+  }
+
+  return sum;
+}
+
+void main(List<String> arguments) {
+  print("Sum Selected Signals: ${getSignals([
+        20,
+        60,
+        100,
+        140,
+        180,
+        220
+      ], false)}");
+  getSignals([41, 81, 121, 161, 201], true);
+}
---
+++ d10/lib/d10.dart
@@ -0,0 +1,97 @@
+import 'dart:collection';
+import 'dart:io';
+
+List<String> readInput() {
+  File file = File('input.txt');
+
+  // sync
+  List<String> lines = file.readAsLinesSync();
+
+  return lines;
+}
+
+HashSet<String> getCharSet(String input) {
+  var result = HashSet<String>();
+
+  for (var i = 0; i < input.length; i++) {
+    result.add(input[i]);
+  }
+  return result;
+}
+
+HashSet<int> getRangeSet(int from, int to) {
+  if (to < from) {
+    int temp = to;
+    to = from;
+    from = temp;
+  }
+
+  var result = HashSet<int>();
+
+  for (var i = from; i <= to; i++) {
+    result.add(i);
+  }
+
+  return result;
+}
+
+class Stack<T> extends Iterable<T> {
+  StackItem<T>? _head;
+
+  void push(T item) {
+    _head = StackItem(item, _head);
+  }
+
+  T pop() {
+    var item = _head?.item;
+
+    if (item == null) {
+      throw RangeError("Stack contains no elements.");
+    } else {
+      _head = _head?.next;
+      return item;
+    }
+  }
+
+  @override
+  Iterator<T> get iterator => StackIterator(_head);
+}
+
+class StackItem<T> {
+  StackItem(this.item, this.next);
+
+  T item;
+  StackItem<T>? next;
+}
+
+class StackIterator<T> extends Iterator<T> {
+  StackItem<T>? _head;
+  T? _current;
+
+  StackIterator(StackItem<T>? head) {
+    _head = head;
+  }
+
+  @override
+  T get current => _current!;
+
+  @override
+  bool moveNext() {
+    _current = _head?.item;
+
+    if (_current == null) {
+      return false;
+    } else {
+      _head = _head?.next;
+
+      return true;
+    }
+  }
+}
+
+class Vector {
+  Vector(this.X, this.Y);
+
+  int X;
+  int Y;
+}
